50ac110f9428cfe2445a67b2f4a8265f1efaca7e
Mike O'Connor <m.k.oconnor53@gmail.com>
2019-02-19 20:17:28 -0600

    Works to reformat data

--- data/dataSorter.js
+++ data/dataSorter.js
@@ -1,23 +1,23 @@
 const fs = require("fs");
 
-let projects = require("./projectData.json");
+let projects = require("./projectDataOld.json");
 let pledges = require("./pledgeData.json");
 
 let newArr = [];
 
 for (let i = 0; i < projects.length; i++) {
-  let pledged = Math.floor(Math.random() * pledges.goal * 0.1) * 10;
+  let pledged = Math.floor(Math.random() * pledges[i].goal * 0.1) * 10;
   let backer_count = Math.floor(pledged / (Math.random() * 90 + 10));
   let newObj = {
-    name: projects.name,
-    creator: projects.creator,
-    creator_img: projects.creator_img,
-    blurb: projects.blurb,
-    thumbnail: projects.thumbnail,
-    full_img: projects.full_img,
-    location: projects.location,
-    category: projects.category,
-    goal: pledges.goal,
+    name: projects[i].name,
+    creator: projects[i].creator,
+    creator_img: projects[i].creator_img,
+    blurb: projects[i].blurb,
+    thumbnail: projects[i].thumbnail,
+    full_img: projects[i].full_img,
+    location: projects[i].location,
+    category: projects[i].category,
+    goal: pledges[i].goal,
     pledged: pledged,
     backer_count: backer_count,
     days_left: Math.floor(Math.random() * 50) + 2
@@ -25,4 +25,4 @@ for (let i = 0; i < projects.length; i++) {
   newArr.push(newObj);
 }
 
-fs.writeFile("projectData1.json", JSON.stringify(newArr), () => {});
+fs.writeFile("projectData.json", JSON.stringify(newArr), () => {});
